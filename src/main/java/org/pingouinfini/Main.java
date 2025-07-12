1e852c58150d2830c4b848852319e6848687c84d
PingouInfini <pingouin@banquise.org>
2025-07-12 02:36:14 +0200

    Fix export de tuiles depuis geoserver

--- src/main/java/org/pingouinfini/Main.java
+++ src/main/java/org/pingouinfini/Main.java
@@ -1,5 +1,6 @@
 package org.pingouinfini;
 
+import gov.nasa.worldwind.geom.LatLon;
 import org.pingouinfini.geojson.*;
 import org.pingouinfini.geojson.Point;
 import org.pingouinfini.geojson.Polygon;
@@ -16,6 +17,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+
+import static gov.nasa.worldwind.geom.LatLon.fromDegrees;
 
 
 public class Main {
@@ -273,27 +276,35 @@ public class Main {
         MapDataExporter.addKmlToLeafletMap(Paths.get("src/main/resources/samples/kml/balade-a-versailles.kml"), outputPath + "/data/data.js");
         MapDataExporter.addKmlToLeafletMap(Paths.get("src/main/resources/samples/kml/arrondissements-paris.kml"), outputPath + "/data/data.js");
 
-        // Etape 3 : Stocker des tuiles (à remplacer par des appels multithread à geoserver sur un layer)
-        Path sourceDir = Paths.get("src/main/resources/samples/tiles");
-        Path targetDir = outputPath.resolve("cache-carto");
-        Files.walk(sourceDir).forEach(source -> {
-            try {
-                Path target = targetDir.resolve(sourceDir.relativize(source));
-                if (Files.isDirectory(source)) {
-                    Files.createDirectories(target);
-                } else {
-                    Files.createDirectories(target.getParent());
-                    Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
+        boolean USE_SAMPLE = true;
+
+        // Etape 3a : Export des tuiles depuis le répertoire sample
+        if (USE_SAMPLE) {
+            Path sourceDir = Paths.get("src/main/resources/samples/tiles");
+            Path targetDir = outputPath.resolve("cache-carto");
+            Files.walk(sourceDir).forEach(source -> {
+                try {
+                    Path target = targetDir.resolve(sourceDir.relativize(source));
+                    if (Files.isDirectory(source)) {
+                        Files.createDirectories(target);
+                    } else {
+                        Files.createDirectories(target.getParent());
+                        Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
+                    }
+                } catch (IOException e) {
+                    throw new UncheckedIOException(e);
                 }
-            } catch (IOException e) {
-                throw new UncheckedIOException(e);
-            }
-        });
-
-        // Etape 4 : export de tuiles
-//        TileDownloader.downloadTiles(0, 15, "MYWORKSPACE:aggregat_0115c6ec-54d1-43af-a727-8f8c17a9d7bd", Paths.get("outputDirectory"));
-
-        // Export GeoJSON
-//        GeoJsonExporter.exportGeoJson(points, polygons, "export.geojson");
+            });
+        } else {
+            // Etape 3b : export de tuiles depuis geoserver
+            List<String> layers = Arrays.asList("s_548b69ac3886e076a5f4a0a8d07f497b__99b4d0a6-c0e3-4c94-8ac1-29e33437b6a1",
+                    "s_577604db5c0db6e34e8ee0d8e8d74040__66336714-d8b9-4472-ba06-7adcff84dcc2",
+                    "s_5648fbcd8e65963604a6b4305e224278__69ddec48-ecb9-4a75-9ee1-2dd096848033");
+            LatLon latLonMin = fromDegrees(48.70374254612109, 1.8136593222179598);
+            LatLon latLonMax = fromDegrees(48.964550947457376, 2.5978297845196505);
+            TileDownloader.downloadTiles(latLonMin, latLonMax, layers, Paths.get("outputDirectory"));
+        }
+        // Etape 4 : Export GeoJSON
+        GeoJsonExporter.exportGeoJson(points, polygons, "export.geojson");
     }
 }
